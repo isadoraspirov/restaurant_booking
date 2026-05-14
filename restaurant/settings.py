f9da15b6a368b5dee67a89ab10b403903fd6d129
isadoraspirov <dora.isinha2@gmail.com>
2026-05-14 21:19:21 +0100

    Update ALLOWED_HOSTS with localhost

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s0ch7b-(a6v4pjx)6mg7hwz-+x)i6ishc58m&90$*v@%m#zxos'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
