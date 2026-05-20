1bd7943b47f487abee3c437743ec39a9b2ad6b81
isadoraspirov <dora.isinha2@gmail.com>
2026-05-20 19:15:37 +0100

    Set up Cloudinary, add base.html structure, and configure favicon

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'homepage',
     'booking',
 ]
